ad9b501b8f9a527c83eb1f0538e08f8183ac457e
Wanda Hayes <nielsen.wh@hotmail.com>
2017-09-13 16:13:52 -0400

    log file change

--- wandaTestZephyr/runNG.bat
+++ wandaTestZephyr/runNG.bat
@@ -1,5 +1,5 @@
 @Echo off
-SET LOGFILE=C:\Users\graywa01\gitZephyrInt\wandaTestZephyr\batchlogfile.log
+SET LOGFILE=C:\WH Things\LogFile\batchlogfile.log
 call :Logit >>%LOGFILE%
 exit / b 0
 :Logit
@@ -7,4 +7,4 @@ set projectpath=C:\Users\graywa01\gitZephyrInt\wandaTestZephyr
 cd %projectpath%
 set classpath=%projectpath%\bin;%projectpath%\libs\*
 java org.testng.TestNG %projectpath%\testng.xml
-pause
+--pause
